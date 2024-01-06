50fa4674f84fef3a1776596174f56f46eaf9c2cd
mirad <50926873+rbxxaxa@users.noreply.github.com>
2024-01-05 23:32:52 -0500

    This time im really going to do it
    (use webassembly)

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,27 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = function override(config, env) {
-    config.resolve.fallback = {
-        ...config.resolve.fallback,
-        "util": require.resolve("util/"),
-        "stream": require.resolve("stream-browserify"),
-        "zlib": require.resolve("browserify-zlib"),
-        "assert": require.resolve("assert"),
-    };
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    util: require.resolve("util/"),
+    stream: require.resolve("stream-browserify"),
+    zlib: require.resolve("browserify-zlib"),
+    assert: require.resolve("assert"),
+  };
 
-    config.plugins.push(
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-            Buffer: ['buffer', 'Buffer'],
-        }),
-    );
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    })
+  );
 
-    return config;
+  config.plugins.push(
+    new CopyPlugin({
+      patterns: [{ from: "src/bleed.wasm", to: "static/js" }],
+    })
+  );
+
+  return config;
 };
