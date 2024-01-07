099942ea7102429a7108b8ad2bce8cc0ee11b8c4
mirad <50926873+rbxxaxa@users.noreply.github.com>
2024-01-07 05:37:46 -0500

    UI final

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Container from "react-bootstrap/Container";
 import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { v4 as uuidv4 } from "uuid";
-import { memo, useEffect } from "react";
+import { memo, useEffect, useRef } from "react";
 import { Row, Col } from "react-bootstrap";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import Worker from "worker-loader!./worker.js";
 import JSZip from "jszip";
 import { saveAs } from "file-saver";
+import { CloseButton } from "react-bootstrap";
 
 // Create a worker pool
 const workerPool = [];
@@ -29,14 +30,19 @@ function onProcessTaskWithWorker() {
     if (task) {
       worker.busy = true;
       worker.onmessage = (e) => {
-        worker.busy = false;
-
-        const { dataUrl } = e.data;
-        task.setDataUrl(dataUrl);
-
-        console.log("tasks left: ", taskQueue.length);
-        if (taskQueue.length > 0) {
-          onProcessTaskWithWorker();
+        if (e.data.error) {
+          worker.busy = false;
+          task.onImageProcessed(true, null);
+          if (taskQueue.length > 0) {
+            onProcessTaskWithWorker();
+          }
+        } else {
+          worker.busy = false;
+          const { dataUrl } = e.data;
+          task.onImageProcessed(false, dataUrl);
+          if (taskQueue.length > 0) {
+            onProcessTaskWithWorker();
+          }
         }
       };
       worker.postMessage(task.file);
@@ -44,90 +50,227 @@ function onProcessTaskWithWorker() {
   }
 }
 
-function processTaskWithWorker(file, setDataUrl) {
-  taskQueue.push({ file, setDataUrl });
+function processTaskWithWorker(file, onImageProcessed) {
+  taskQueue.push({ file, onImageProcessed });
   onProcessTaskWithWorker();
 }
 
-function exportImages(images) {
-  const zip = new JSZip();
-  const filenames = {};
+const exportImages = async (images, setIsExportInProgress) => {
+  setIsExportInProgress(true);
 
-  Object.entries(images).forEach(([id, { filename, dataUrl }]) => {
-    const base64Data = dataUrl.replace(/^data:image\/(png|jpg);base64,/, "");
+  // Short delay here so that react has time to update the UI
+  await new Promise((resolve) => setTimeout(resolve, 100));
 
-    // Check for duplicate filenames
-    if (filenames[filename]) {
-      const dotIndex = filename.lastIndexOf(".");
-      const name = filename.substring(0, dotIndex);
-      const extension = filename.substring(dotIndex);
-      filename = `${name}(${filenames[filename]})${extension}`;
-      filenames[filename]++;
-    } else {
-      filenames[filename] = 1;
-    }
+  const entries = Object.entries(images);
+  const validEntries = entries.filter(
+    ([_id, entry]) => !entry.isInvalid && entry.isProcessed
+  );
 
-    zip.file(filename, base64Data, { base64: true });
-  });
+  if (validEntries.length === 1) {
+    // If there's only one file, download it as a png
+    const [_id, { filename, _dataUrl, _isProcessed, processedDataUrl }] =
+      validEntries[0];
+    const link = document.createElement("a");
+    link.href = processedDataUrl;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } else {
+    // If there's more than one file, proceed with creating a zip
+    const zip = new JSZip();
+    const filenames = {};
 
-  zip.generateAsync({ type: "blob" }).then((content) => {
+    validEntries.forEach(
+      ([_id, { filename, _dataUrl, isProcessed, processedDataUrl }]) => {
+        // Check for duplicate filenames
+        if (filenames[filename]) {
+          const dotIndex = filename.lastIndexOf(".");
+          const name = filename.substring(0, dotIndex);
+          const extension = filename.substring(dotIndex);
+          filename = `${name}(${filenames[filename]})${extension}`;
+          filenames[filename]++;
+        } else {
+          filenames[filename] = 1;
+        }
+
+        const base64Data = processedDataUrl.replace(
+          /^data:image\/(png|jpg);base64,/,
+          ""
+        );
+        zip.file(filename, base64Data, { base64: true });
+      }
+    );
+
+    const content = await zip.generateAsync({ type: "blob" });
     saveAs(content, "images.zip");
-  });
-}
+  }
 
-function ImageEntry({ imageUrl, fileName, removeImage }) {
+  setIsExportInProgress(false);
+};
+
+function ImageEntry({
+  id,
+  dataUrl,
+  fileName,
+  isProcessed,
+  isInvalid,
+  processedDataUrl,
+  removeImage,
+}) {
   return (
     <div
       style={{
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        marginBottom: "1em",
+        marginBottom: "0.5em",
+        marginTop: "0.5em",
+        padding: "0.5em",
+        boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.4)",
+        height: "4em",
       }}
     >
-      <img
-        src={imageUrl}
-        alt={fileName}
-        style={{ width: "100px", height: "100px", objectFit: "cover" }}
+      <div height="4em">
+        <img
+          src={isProcessed ? processedDataUrl : dataUrl}
+          alt={fileName}
+          draggable="false"
+          style={{
+            width: "3em",
+            height: "3em",
+            objectFit: "cover",
+            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.8)",
+            marginRight: "1em",
+            opacity: isProcessed ? 1 : 0.5,
+          }}
+        />
+
+        <div style={{ display: "inline", alignItems: "center" }}>
+          <span
+            style={{
+              textAlign: "left",
+              whiteSpace: "nowrap",
+              textDecoration: isInvalid ? "line-through" : "none",
+            }}
+          >
+            {fileName}
+          </span>
+          {isInvalid && (
+            <span
+              style={{
+                marginLeft: "10px",
+                color: "red",
+              }}
+            >
+              Cannot be processed
+            </span>
+          )}
+        </div>
+      </div>
+      <CloseButton
+        variant="white"
+        style={{ paddingRight: "2em" }}
+        height="1em"
+        width="1em"
+        onClick={() => removeImage(id)}
+        aria-label="Remove"
       />
-      <span>{fileName}</span>
-      <button onClick={() => removeImage(fileName)}>Remove</button>
     </div>
   );
 }
+ImageEntry = memo(ImageEntry);
 
 function App() {
   const [images, setImages] = useState({});
   const [isDragActive, setDragActive] = useState(false);
+  const [isExportInProgress, setIsExportInProgress] = useState(false);
 
-  const addImage = useCallback((id, filename, dataUrl) => {
-    setImages((prevState) => ({ ...prevState, [id]: { filename, dataUrl } }));
-  }, []);
+  const addImage = useCallback(
+    (id, filename, dataUrl) => {
+      setImages((prevState) => ({
+        ...prevState,
+        [id]: {
+          filename,
+          dataUrl,
+          isProcessed: false,
+          isInvalid: false,
+          processedDataUrl: null,
+        },
+      }));
+    },
+    [setImages]
+  );
 
-  const removeImage = useCallback((id) => {
-    setImages((prevState) => {
-      const newState = { ...prevState };
-      delete newState[id];
-      return newState;
-    });
-  }, []);
+  const removeImage = useCallback(
+    (id) => {
+      setImages((prevState) => {
+        const newState = { ...prevState };
+        delete newState[id];
+        return newState;
+      });
+    },
+    [setImages]
+  );
 
-  const onExport = useCallback(() => {
-    exportImages(images);
-  }, [images]);
+  const imageCount = Object.keys(images).length;
+  const processedImageCount = Object.values(images).filter(
+    (image) => image.isProcessed
+  ).length;
+  const invalidImageCount = Object.values(images).filter(
+    (image) => image.isInvalid
+  ).length;
+  const validImageCount = imageCount - invalidImageCount;
+
+  const onImageProcessed = useCallback(
+    (id, isInvalid, processedDataUrl) => {
+      setImages((prevState) => {
+        if (prevState[id]) {
+          if (!isInvalid) {
+            const newState = { ...prevState };
+            newState[id].isProcessed = true;
+            newState[id].processedDataUrl = processedDataUrl;
+            return newState;
+          } else {
+            const newState = { ...prevState };
+            newState[id].isProcessed = true;
+            newState[id].isInvalid = true;
+            newState[id].processedDataUrl = null;
+            return newState;
+          }
+        } else {
+          return prevState;
+        }
+      });
+    },
+    [setImages]
+  );
+
+  const onImageProcessedRef = useRef(onImageProcessed);
+  // Need to store in a ref because onImageProcessed relies on an image and
+  // images are processed asynchronously
+  useEffect(() => {
+    onImageProcessedRef.current = onImageProcessed;
+  }, [onImageProcessed]);
 
   const onDrop = useCallback(
     (acceptedFiles) => {
       acceptedFiles.forEach((file) => {
         const id = uuidv4();
-        processTaskWithWorker({ file: file }, (dataUrl) => {
-          addImage(id, file.name, dataUrl);
+        const dataUrl = URL.createObjectURL(file);
+        addImage(id, file.name, dataUrl);
+        processTaskWithWorker({ file: file }, (isInvalid, processedDataUrl) => {
+          onImageProcessedRef.current(id, isInvalid, processedDataUrl);
         });
       });
       setDragActive(false);
     },
-    [addImage, setDragActive]
+    [addImage]
   );
+
+  const onExport = useCallback(() => {
+    exportImages(images, setIsExportInProgress);
+  }, [images, exportImages, setIsExportInProgress]);
 
   const { getRootProps, getInputProps, open } = useDropzone({
     onDrop,
@@ -164,7 +307,13 @@ function App() {
         <input {...getInputProps()} />
       </div>
 
-      <div className={isDragActive ? "drag-blur" : ""}>
+      <div
+        className={isDragActive ? "drag-blur" : ""}
+        style={{
+          transition: "filter 0.1s ease",
+          filter: isDragActive ? "blur(5px)" : "none",
+        }}
+      >
         <div class="container-md">
           <h1 className="display-1 text-center title" aria-label="chipng">
             <span className="red-text">血chi</span>png
@@ -191,44 +340,94 @@ function App() {
               </a>
             </sup>
           </p>
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={open}
-            aria-label="select .png files"
+
+          <div className="button-container">
+            <div
+              style={{
+                flex: 1,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={open}
+                aria-label="select .png files"
+              >
+                select .png files
+              </button>
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                flex: 1,
+              }}
+            >
+              <span style={{ alignSelf: "center", margin: "0.5em" }}>
+                and then
+              </span>{" "}
+            </div>
+
+            <div
+              style={{
+                flex: 1,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={onExport}
+                aria-label="export"
+                disabled={
+                  isExportInProgress ||
+                  validImageCount === 0 ||
+                  processedImageCount !== imageCount
+                }
+              >
+                {processedImageCount !== imageCount
+                  ? `processing ${processedImageCount} / ${imageCount} images...`
+                  : validImageCount == 0
+                  ? "export!"
+                  : invalidImageCount == 0
+                  ? `export ${validImageCount} images!`
+                  : `export ${validImageCount} images! (${invalidImageCount} invalid)`}
+              </button>
+            </div>
+
+            <small className="text-muted italic">
+              (you can also drag images into this window!)
+            </small>
+          </div>
+
+          <div
+            style={{ paddingTop: "2em", maxWidth: "800px", margin: "0 auto" }}
           >
-            select .png files
-          </button>
-          or simply drag them into this window.
-          <br />
-          afterwards
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={onExport}
-            aria-label="export"
-          >
-            export
-          </button>{" "}
-          them into your computer.
-          {/* <div
-            className="container.fluid"
-            style={{
-              backgroundColor: "#FFFFFF",
-              height: "70vh",
-              overflowY: "auto",
-              minHeight: "400px",
-              pointerEvents: "none",
-            }}
-          ></div> */}
-          {/* {Object.entries(images).map(([id, { filename, dataUrl }]) => (
-              <ImageEntry
-                key={id}
-                imageUrl={dataUrl}
-                fileName={filename}
-                removeImage={removeImage}
-              />
-            ))} */}
+            {Object.entries(images).map(
+              ([id, { filename, dataUrl, isProcessed, isInvalid }]) => (
+                <ImageEntry
+                  key={id}
+                  id={id}
+                  dataUrl={dataUrl}
+                  fileName={filename}
+                  isProcessed={isProcessed}
+                  isInvalid={isInvalid}
+                  processedDataUrl={dataUrl}
+                  removeImage={removeImage}
+                />
+              )
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/worker.js
+++ src/worker.js
@@ -6,139 +6,145 @@ import { PNG } from "pngjs";
 addEventListener("message", (e) => {
   const { file } = e.data;
 
-  file.arrayBuffer().then((buffer) => {
-    const png = new PNG();
-    png.parse(buffer, (err) => {
-      let data = png.data;
-      if (err) throw err;
-      const { width, height } = png;
+  file
+    .arrayBuffer()
+    .then((buffer) => {
+      const png = new PNG();
+      png.parse(buffer, (err) => {
+        let data = png.data;
+        if (err) throw err;
+        const { width, height } = png;
 
-      let opaque = new Uint8Array(width * height);
-      let loose = new Uint8Array(width * height);
-      let pending = [];
-      let pendingNext = [];
-      let offsets = [
-        [-1, -1],
-        [0, -1],
-        [1, -1],
-        [-1, 0],
-        [1, 0],
-        [-1, 1],
-        [0, 1],
-        [1, 1],
-      ];
+        let opaque = new Uint8Array(width * height);
+        let loose = new Uint8Array(width * height);
+        let pending = [];
+        let pendingNext = [];
+        let offsets = [
+          [-1, -1],
+          [0, -1],
+          [1, -1],
+          [-1, 0],
+          [1, 0],
+          [-1, 1],
+          [0, 1],
+          [1, 1],
+        ];
 
-      for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-          let idx = width * y + x;
-          let a = data[idx * 4 + 3];
-          if (a == 0) {
-            let isLoose = true;
+        for (let y = 0; y < height; y++) {
+          for (let x = 0; x < width; x++) {
+            let idx = width * y + x;
+            let a = data[idx * 4 + 3];
+            if (a == 0) {
+              let isLoose = true;
 
-            for (let k = 0; k < 8; k++) {
-              let s = offsets[k][0];
-              let t = offsets[k][1];
-              let nX = x + s;
-              let nY = y + t;
-              let nIdx = width * nY + nX;
-              if (nIdx >= 0 && nIdx < width * height) {
-                let neighborAlpha = data[nIdx * 4 + 3];
+              for (let k = 0; k < 8; k++) {
+                let s = offsets[k][0];
+                let t = offsets[k][1];
+                let nX = x + s;
+                let nY = y + t;
+                let nIdx = width * nY + nX;
+                if (nIdx >= 0 && nIdx < width * height) {
+                  let neighborAlpha = data[nIdx * 4 + 3];
 
-                if (neighborAlpha != 0) {
-                  isLoose = false;
-                  break;
+                  if (neighborAlpha != 0) {
+                    isLoose = false;
+                    break;
+                  }
                 }
               }
-            }
 
-            if (!isLoose) {
-              pending.push({ x: x, y: y });
+              if (!isLoose) {
+                pending.push({ x: x, y: y });
+              } else {
+                loose[idx] = 1;
+              }
             } else {
-              loose[idx] = 1;
+              opaque[idx] = 0xff;
             }
-          } else {
-            opaque[idx] = 0xff;
-          }
-        }
-      }
-
-      while (pending.length > 0) {
-        pendingNext = [];
-
-        for (let p = 0; p < pending.length; p++) {
-          let coord = pending[p];
-          let x = coord.x;
-          let y = coord.y;
-          let idx = width * y + x;
-
-          let r = 0;
-          let g = 0;
-          let b = 0;
-
-          let count = 0;
-
-          for (let k = 0; k < 8; k++) {
-            let s = offsets[k][0];
-            let t = offsets[k][1];
-            let nX = x + s;
-            let nY = y + t;
-            let nIdx = width * nY + nX;
-            if (nIdx >= 0 && nIdx < width * height) {
-              if (opaque[nIdx] & 1) {
-                r += data[nIdx * 4 + 0];
-                g += data[nIdx * 4 + 1];
-                b += data[nIdx * 4 + 2];
-
-                count++;
-              }
-            }
-          }
-
-          if (count > 0) {
-            data[idx * 4 + 0] = r / count;
-            data[idx * 4 + 1] = g / count;
-            data[idx * 4 + 2] = b / count;
-            data[idx * 4 + 3] = 0;
-            opaque[idx] = 0xfe;
-
-            for (let k = 0; k < 8; k++) {
-              let s = offsets[k][0];
-              let t = offsets[k][1];
-              let nX = x + s;
-              let nY = y + t;
-              let nIdx = width * nY + nX;
-
-              if (nIdx >= 0 && nIdx < width * height) {
-                if (loose[nIdx]) {
-                  pendingNext.push({ x: nX, y: nY });
-                  loose[nIdx] = 0;
-                }
-              }
-            }
-          } else {
-            pendingNext.push({ x: x, y: y });
           }
         }
 
-        if (pendingNext.length > 0) {
+        while (pending.length > 0) {
+          pendingNext = [];
+
           for (let p = 0; p < pending.length; p++) {
             let coord = pending[p];
-            let idx = width * coord.y + coord.x;
-            opaque[idx] >>= 1;
+            let x = coord.x;
+            let y = coord.y;
+            let idx = width * y + x;
+
+            let r = 0;
+            let g = 0;
+            let b = 0;
+
+            let count = 0;
+
+            for (let k = 0; k < 8; k++) {
+              let s = offsets[k][0];
+              let t = offsets[k][1];
+              let nX = x + s;
+              let nY = y + t;
+              let nIdx = width * nY + nX;
+              if (nIdx >= 0 && nIdx < width * height) {
+                if (opaque[nIdx] & 1) {
+                  r += data[nIdx * 4 + 0];
+                  g += data[nIdx * 4 + 1];
+                  b += data[nIdx * 4 + 2];
+
+                  count++;
+                }
+              }
+            }
+
+            if (count > 0) {
+              data[idx * 4 + 0] = r / count;
+              data[idx * 4 + 1] = g / count;
+              data[idx * 4 + 2] = b / count;
+              data[idx * 4 + 3] = 0;
+              opaque[idx] = 0xfe;
+
+              for (let k = 0; k < 8; k++) {
+                let s = offsets[k][0];
+                let t = offsets[k][1];
+                let nX = x + s;
+                let nY = y + t;
+                let nIdx = width * nY + nX;
+
+                if (nIdx >= 0 && nIdx < width * height) {
+                  if (loose[nIdx]) {
+                    pendingNext.push({ x: nX, y: nY });
+                    loose[nIdx] = 0;
+                  }
+                }
+              }
+            } else {
+              pendingNext.push({ x: x, y: y });
+            }
           }
+
+          if (pendingNext.length > 0) {
+            for (let p = 0; p < pending.length; p++) {
+              let coord = pending[p];
+              let idx = width * coord.y + coord.x;
+              opaque[idx] >>= 1;
+            }
+          }
+
+          pending = pendingNext;
         }
 
-        pending = pendingNext;
-      }
-
-      const resultBuffer = PNG.sync.write(png);
-      const blob = new Blob([resultBuffer], { type: "image/png" });
-      const reader = new FileReader();
-      reader.onload = function (event) {
-        const dataUrl = event.target.result;
-        postMessage({ dataUrl: dataUrl });
-      };
-      reader.readAsDataURL(blob);
+        const resultBuffer = PNG.sync.write(png);
+        const blob = new Blob([resultBuffer], { type: "image/png" });
+        const reader = new FileReader();
+        reader.onload = function (event) {
+          const dataUrl = event.target.result;
+          postMessage({ dataUrl: dataUrl });
+        };
+        reader.readAsDataURL(blob);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      postMessage({ error: error.toString() });
     });
-  });
 });
